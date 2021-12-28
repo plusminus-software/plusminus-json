cb55710bd8e38121ee5847388b9ec9964c52004a
Taras Shpek <taras.shpek@gmail.com>
2021-12-28 22:23:28 +0200

    make ClassableTypescriptGenerator supportable only for Classable

--- src/main/java/software/plusminus/generator/ClassableTypescriptGenerator.java
+++ src/main/java/software/plusminus/generator/ClassableTypescriptGenerator.java
@@ -3,6 +3,7 @@ package software.plusminus.generator;
 import org.springframework.stereotype.Component;
 import software.plusminus.generator.typescript.TypescriptGenerator;
 import software.plusminus.generator.typescript.model.TypescriptClass;
+import software.plusminus.json.model.Classable;
 
 @Component
 public class ClassableTypescriptGenerator extends TypescriptGenerator {
@@ -13,5 +14,10 @@ public class ClassableTypescriptGenerator extends TypescriptGenerator {
         typescriptClass.fields().add("class: string;");
         typescriptClass.constructor("  this.class = this.constructor.name;");
         return typescriptClass;
+    }
+
+    @Override
+    public boolean supports(Class<?> sourceClass) {
+        return sourceClass == Classable.class;
     }
 }
