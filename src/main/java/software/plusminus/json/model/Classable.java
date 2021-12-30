96d632f7edacdf36729bf76f6153cb01b065d06f
Taras Shpek <taras.shpek@gmail.com>
2021-12-30 23:18:06 +0200

    remove plusminus-generator dependency

--- src/main/java/software/plusminus/json/model/Classable.java
+++ src/main/java/software/plusminus/json/model/Classable.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
-import software.plusminus.generator.ClassableTypescriptGenerator;
-import software.plusminus.generator.Generate;
 import software.plusminus.json.config.BeanIdResolver;
 
 @JsonPropertyOrder("class")
@@ -13,7 +11,6 @@ import software.plusminus.json.config.BeanIdResolver;
         include = JsonTypeInfo.As.EXISTING_PROPERTY,
         property = "class")
 @JsonTypeIdResolver(BeanIdResolver.class)
-@Generate(ClassableTypescriptGenerator.class)
 public interface Classable {
 
     @JsonProperty("class")
